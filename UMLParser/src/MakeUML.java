e8896b803121390e1213bd8e4727b6c2f212992a
kunal312 <kunal312@hotmail.com>
2017-03-27 19:13:56 -0700

    Making Request to yUML API

--- UMLParser/src/MakeUML.java
+++ UMLParser/src/MakeUML.java
@@ -1,4 +1,48 @@
 
-public class MakeUML {
+import java.io.*;
+import java.net.*;
 
+public class MakeUML {
+	
+	public Boolean makeUML(String parseString, String dst_Path){
+	
+		String base_yUML_API_Link="https://yuml.me/diagram/scruffy/class/";
+		String complete_yUML_Link= base_yUML_API_Link+ parseString+".jpg";
+		HttpURLConnection connection = null;
+		//Creating HTTP Connection
+		try{
+			System.out.println("Generating Class diargamfrom the given Grammar. Making request to yUML API");
+			URL url = new URL(complete_yUML_Link);
+		//HTTP Request
+			 connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Accpet", "application/json");
+			connection.setUseCaches(false);
+			connection.setDoOutput(true);
+			if(connection.getResponseCode()!=200){
+				throw new RuntimeException("Cannot Generate Diagram. Request Failed.Error Code:" + connection.getResponseCode());
+			}
+			
+		//Reading and Saving jpg file to destination path
+		OutputStream op = new FileOutputStream(new File(dst_Path));
+		byte[] bytes = new byte[1024];
+		int read=0;
+		while((read=connection.getInputStream().read(bytes))!=-1){
+			op.write(bytes, 0, read);
+		}
+		op.close();
+		
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}finally{
+			if(connection!=null){
+				connection.disconnect();
+			}
+		}
+		
+	return null;
+
+	}
+	
 }
