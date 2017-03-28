f32cb6e8d4955e8cde2e8a69f640310f73834fc9
kunal312 <kunal312@hotmail.com>
2017-03-28 02:12:17 -0700

    Made the code modular. Created separate method for HTTP Connection which accepts URL as an argument and returns true if response code received was 200.

--- UMLParser/src/MakeUML.java
+++ UMLParser/src/MakeUML.java
@@ -4,48 +4,125 @@ import java.net.*;
 
 public class MakeUML {
 	
+	HttpURLConnection connection = null;
+	private static final int BUFFER_SIZE = 4096;
 	
 	public Boolean makeUML(String parseString, String dst_Path){
 		
 		String base_yUML_API_Link="https://yuml.me/diagram/plain/class/";
+//		String base_yUML_API_Link="https://yuml.me/diagram/nofunky/class/";		
+//		String base_yUML_API_Link="https://yuml.me/diagram/scruffy/class/";
 		String complete_yUML_Link= base_yUML_API_Link+ parseString;
 		System.out.println("Complete Link: "+complete_yUML_Link);
-		HttpURLConnection connection = null;
+		
+		
 		//Creating HTTP Connection
 		try{
-			System.out.println("Generating Class diargamfrom the given Grammar. Making request to yUML API");
-			URL url = new URL(complete_yUML_Link);
-		//HTTP Request
-			 connection = (HttpURLConnection) url.openConnection();
-			connection.setRequestMethod("GET");
-			connection.setRequestProperty("Accept", "application/json");
-			//connection.setUseCaches(false);
-			//connection.setDoOutput(true);
-			if(connection.getResponseCode()!=200){
-				throw new RuntimeException("Cannot Generate Diagram. Request Failed.Error Code:" + connection.getResponseCode());
-			}
 			
-		//Reading and Saving jpg file to destination path
-		OutputStream op = new FileOutputStream(new File(dst_Path));
-		byte[] bytes = new byte[1024];
-		int read=0;
-		while((read=connection.getInputStream().read(bytes))!=-1){
-			op.write(bytes, 0, read);
-		}
-		op.close();
-		
+			System.out.println("Generating Class diargamfrom the given Grammar. Making request to yUML API");
+		    if(connectToHTTP(complete_yUML_Link))
+		    {
+		    	//Reading and Saving jpg file to destination path
+				InputStream inputStream = connection.getInputStream();
+				FileOutputStream outputStream = new FileOutputStream(new File(dst_Path));
+				//OutputStream op = new FileOutputStream(new File(dst_Path));
+				int read_bytes=-1;
+				byte[] buffer = new byte[BUFFER_SIZE];
+				while((read_bytes=inputStream.read(buffer))!=-1){
+					outputStream.write(buffer, 0, read_bytes);
+				}
+				
+				outputStream.close();
+				inputStream.close();
+				System.out.println("Class diargam generated");
+		    }		    	
 		}catch(Exception e){
 			e.printStackTrace();
 			return null;
 		}finally{
 			
-			if(connection!=null){
-				connection.disconnect();
+			 	if(connection!=null){
+			 		try{
+			 			connection.disconnect();
+			 		}
+			 		catch(Exception e){
+			 			e.printStackTrace();
+			 		}
+				
 			}
 		}
-		
-	return null;
+	
+		return null;
 
+}
+	
+	//Method for making connection to yUML API. Returns true if response code is 200
+	private boolean connectToHTTP(String complete_Url){
+		try{
+		URL url = new URL(complete_Url);
+		
+		//HTTP Request
+		connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("GET");
+		connection.setRequestProperty("Accept", "application/json");
+		
+		int responseCode = connection.getResponseCode();
+		if(responseCode==200){
+			System.out.println("Response code:200");
+			return true;
+		}else{
+			throw new RuntimeException("Cannot Generate Diagram. Request Failed.Error Code:" + responseCode);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	return false;
 	}
 	
 }
+		
+//URL url = new URL(complete_yUML_Link);
+////HTTP Request
+//connection = (HttpURLConnection) url.openConnection();
+//connection.setRequestMethod("GET");
+//connection.setRequestProperty("Accept", "application/json");
+//
+//int responseCode = connection.getResponseCode();
+//
+//
+//if(responseCode!=200){
+//	throw new RuntimeException("Cannot Generate Diagram. Request Failed.Error Code:" + responseCode);
+//}else{
+////Reading and Saving jpg file to destination path
+//InputStream inputStream = connection.getInputStream();
+//FileOutputStream outputStream = new FileOutputStream(new File(dst_Path));
+////OutputStream op = new FileOutputStream(new File(dst_Path));
+//int read_bytes=-1;
+//byte[] buffer = new byte[BUFFER_SIZE];
+//while((read_bytes=inputStream.read(buffer))!=-1){
+//	outputStream.write(buffer, 0, read_bytes);
+//}
+//
+//outputStream.close();
+//inputStream.close();
+//
+//System.out.println("Class diargam generated");
+//	
+//}
+//
+//}catch(Exception e){
+//e.printStackTrace();
+//return null;
+//}finally{
+//
+// 	if(connection!=null){
+// 		try{
+// 			connection.disconnect();
+// 		}
+// 		catch(Exception e){
+// 			e.printStackTrace();
+// 		}
+//	
+//}
+//}
--- UMLParser/src/UMLParserMain.java
+++ UMLParser/src/UMLParserMain.java
@@ -5,7 +5,7 @@ public class UMLParserMain {
 		
 		MakeUML mu = new MakeUML();
 		String parseString = "[Customer|-forname:string;surname:string|doShiz()]<>-orders*>[Order], [Order]++-0..*>[LineItem], [Order]-[note:Aggregate root{bg:wheat}]";
-		String dst_Path = "test_diagram1234.jpg";
+		String dst_Path = "test_diagram255657654.jpg";
 		mu.makeUML(parseString, dst_Path);
 	}
 	
